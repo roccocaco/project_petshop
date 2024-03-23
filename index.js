8da40805f61318426dc3fbc7ab6b10dcba01c841
Alberto <betim009@gmail.com>
2024-03-23 10:37:44 -0300

    refatorando saldo

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import { dataClientes } from "./src/data/dataClientes.js";
 
 let saldo = 0;
-let saldo2 = 0;
-let saldo3 = 0;
-let saldo4 = 0;
-let saldo5 = 0;
 
 for (let index = 0; index < dataClientes.length; index++) {
     if (dataClientes[index].tipoDeMensalidade === 1) {
@@ -12,20 +8,20 @@ for (let index = 0; index < dataClientes.length; index++) {
     }
 
     if (dataClientes[index].tipoDeMensalidade === 2) {
-        saldo2 += 49.99
+        saldo += 49.99
     }
 
     if (dataClientes[index].tipoDeMensalidade === 3) {
-        saldo3 += 24.99
+        saldo += 24.99
     }
 
     if (dataClientes[index].tipoDeMensalidade === 4) {
-        saldo4 += 34.99
+        saldo += 34.99
     }
 
     if (dataClientes[index].tipoDeMensalidade === 5) {
-        saldo5 += 44.99
+        saldo += 44.99
     }
 
 }
-console.log(saldo5)
+console.log(saldo)
