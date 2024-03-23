2f838cfb2b8d712cbb061dc650434a5098ba90d2
Alberto <betim009@gmail.com>
2024-03-23 10:36:09 -0300

    corringindo a base de dados

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-import { clientes } from "./data.js";
+import { dataClientes } from "./src/data/dataClientes.js";
 
-console.log(clientes)
+let saldo = 0;
+let saldo2 = 0;
+let saldo3 = 0;
+let saldo4 = 0;
+let saldo5 = 0;
+
+for (let index = 0; index < dataClientes.length; index++) {
+    if (dataClientes[index].tipoDeMensalidade === 1) {
+        saldo += 14.99
+    }
+
+    if (dataClientes[index].tipoDeMensalidade === 2) {
+        saldo2 += 49.99
+    }
+
+    if (dataClientes[index].tipoDeMensalidade === 3) {
+        saldo3 += 24.99
+    }
+
+    if (dataClientes[index].tipoDeMensalidade === 4) {
+        saldo4 += 34.99
+    }
+
+    if (dataClientes[index].tipoDeMensalidade === 5) {
+        saldo5 += 44.99
+    }
+
+}
+console.log(saldo5)
--- src/data/dataClientes.js
+++ src/data/dataClientes.js
@@ -1,9 +1,3 @@
-let saldo = 0;
-let saldo2 = 0;
-let saldo3 = 0;
-let saldo4 = 0;
-let saldo5 = 0;
-
 export const dataClientes = [
   {
     idCliente: 1,
@@ -25,7 +19,7 @@ export const dataClientes = [
     idCliente: 3,
     cliente: "Marcos Novaes",
     telefone: "(11) 41553748",
-    pets: [3], 
+    pets: [3],
     tipoDeMensalidade: 1,
   },
 
@@ -34,7 +28,7 @@ export const dataClientes = [
     cliente: "Antenor Santana",
     telefone: "(11) 41578959",
     pets: [4],
-    tipoDeMensalidade: 1,
+    tipoDeMensalidade: 5,
   },
 
   {
@@ -42,14 +36,14 @@ export const dataClientes = [
     cliente: "Raquel Ribeiro",
     telefone: "(11) 41593168",
     pets: [5],
-    tipoDeMensalidade: 1, 
+    tipoDeMensalidade: 1,
   },
 
   {
     idCliente: 6,
     cliente: "Chaves",
     telefone: "(11) 41596578",
-    pets: [6], 
+    pets: [6],
     tipoDeMensalidade: 3,
   },
 
@@ -58,7 +52,7 @@ export const dataClientes = [
     cliente: "Paulo Costa",
     telefone: "(11) 41567515",
     pets: [7],
-    tipoDeMensalidade: 3, 
+    tipoDeMensalidade: 3,
   },
 
   {
@@ -66,35 +60,6 @@ export const dataClientes = [
     cliente: "Vanessa",
     telefone: "(11) 41514897",
     pets: [8],
-    tipoDeMensalidade: 5, 
+    tipoDeMensalidade: 5,
   },
 ]
-
-
-
-
-
-
-for (let index = 0; index < clientes.length; index++) {
-    if(clientes[index].tipoDeMensalidade === 1){
-      saldo += 14.99
-    }
-
-    if(clientes[index].tipoDeMensalidade === 2){
-      saldo2 += 49.99
-    }
-
-    if(clientes[index].tipoDeMensalidade === 3){
-      saldo3 += 24.99
-    }
-
-    if(clientes[index].tipoDeMensalidade === 4){
-      saldo4 += 34.99
-    }
-
-    if(clientes[index].tipoDeMensalidade === 5){
-      saldo5 += 44.99
-    }
-     
-}
-console.log(saldo5)
