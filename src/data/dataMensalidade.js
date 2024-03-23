389d66ede5d3694ea6611b7b56bd5a3f40538cbe
Alberto <betim009@gmail.com>
2024-03-23 11:04:04 -0300

    adicionando mais uma base de dados

--- src/data/dataMensalidade.js
+++ src/data/dataMensalidade.js
@@ -2,31 +2,26 @@ export const dataMensalidade = [
     {
         idMensalidade: 1,
         pet: "gato",
-        tipoDeMensalidade: 1,
         preco: 14.99,
     },
     {
         idMensalidade: 2,
         pet: "ambos",
-        tipoDeMensalidade: 2,
         preco: 49.99,
     },
     {
         idMensalidade: 3,
         pet: "cachorro P",
-        tipoDeMensalidade: 3,
         preco: 24.99,
     },
     {
         idMensalidade: 4,
         pet: "cachorro M",
-        tipoDeMensalidade: 4,
         preco: 34.99,
     },
     {
         idMensalidade: 5,
         pet: "cachorro G",
-        tipoDeMensalidade: 5,
         preco: 44.99,
     },
 ]
