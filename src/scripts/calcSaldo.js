903a37980a1f0a311a5e469e3602134387229569
Rocco <144187511+roccocaco@users.noreply.github.com>
2024-03-30 10:45:24 -0300

    Add files via upload

--- src/scripts/calcSaldo.js
+++ src/scripts/calcSaldo.js
@@ -4,23 +4,23 @@ export function calcSaldo() {
     let saldo = 0;
 
     for (let index = 0; index < dataClientes.length; index++) {
-        if (dataClientes[index].tipoDeMensalidade === 1) {
+        if (dataClientes[index].idMensalidade === 1) {
             saldo += 14.99
         };
 
-        if (dataClientes[index].tipoDeMensalidade === 2) {
+        if (dataClientes[index].idMensalidade === 2) {
             saldo += 49.99
         };
 
-        if (dataClientes[index].tipoDeMensalidade === 3) {
+        if (dataClientes[index].idMensalidade === 3) {
             saldo += 24.99
         };
 
-        if (dataClientes[index].tipoDeMensalidade === 4) {
+        if (dataClientes[index].idMensalidade === 4) {
             saldo += 34.99
         };
 
-        if (dataClientes[index].tipoDeMensalidade === 5) {
+        if (dataClientes[index].idMensalidade === 5) {
             saldo += 44.99
         };
 
