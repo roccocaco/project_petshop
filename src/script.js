39455525212fb353fd16c1dc5ed143f15c196504
Rocco Caco <32roccocaco@gmail.com>
2024-04-08 21:14:19 -0300

    atualizando o card

--- src/script.js
+++ src/script.js
@@ -17,13 +17,14 @@ function createElementsServicos() {
     <div class="col-sm-4 m-5 text-center">
 
     <div class="card text-white bg-dark" style="width: 18rem;">
-  <img src=${e.url}>
-  <div class="card-body">
-    <h5 class="card-text text-warning">${e.cliente}</h5>
-    <h6 class="card-text">${e.servico} para ${e.pet}</h6>
-    <p class="card-text">Valor do serviço: R$ ${e.preco}</p>
-  </div>
-</div>
+     
+    <div class="card-body">
+     <img src="${e.url}" class="card-img-top" alt="...">
+      <h5 class="card-text text-warning">${e.cliente}</h5>
+      <h6 class="card-text">${e.servico} para ${e.pet}</h6>
+      <p class="card-text">Valor do serviço: R$ ${e.preco}</p>
+     </div>
+    </div>
 
   `)
   );
