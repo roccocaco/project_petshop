1698da681c3c61c9c17e71645cbeb517903561c1
Alberto <betim009@gmail.com>
2024-04-23 20:47:14 -0300

    caminhos das imagens

--- data/dataServicos.js
+++ data/dataServicos.js
@@ -5,7 +5,7 @@ export const dataServicos = [
         servico: "Banho e higienização para gatos de todos os portes",
         preco: 49.99,
         cliente: false,
-        img: "/src/assets/1.png"
+        img: "assets/1.png"
     },
     {
         idServico: 2,
@@ -13,7 +13,7 @@ export const dataServicos = [
         servico: "Banho e higienização para gatos de todos os portes",
         preco: 24.99,
         cliente: true,
-        img: "/src/assets/2.png"
+        img: "assets/2.png"
     },
     {
         idServico: 3,
@@ -21,7 +21,7 @@ export const dataServicos = [
         servico: "Tosa para cachorro do porte pequeno",
         preco: 99.99,
         cliente: false,
-        img: "/src/assets/4.png"
+        img: "assets/4.png"
     },
     {
         idServico: 4,
@@ -29,7 +29,7 @@ export const dataServicos = [
         servico: "Tosa para cachorro do porte pequeno",
         preco: 49.99,
         cliente: true,
-        img: "/src/assets/3.png"
+        img: "assets/3.png"
     },
     {
         idServico: 5,
@@ -37,7 +37,7 @@ export const dataServicos = [
         servico: "Tosa para cachorro do porte médio",
         preco: 149.99,
         cliente: false,
-        img: "/src/assets/5.png"
+        img: "assets/5.png"
     },
     {
         idServico: 6,
@@ -45,7 +45,7 @@ export const dataServicos = [
         servico: "Tosa para cachorro do porte médio",
         preco: 74.99,
         cliente: true,
-        img: "/src/assets/6.png"
+        img: "assets/6.png"
     },
     {
         idServico: 7,
@@ -53,7 +53,7 @@ export const dataServicos = [
         servico: "Tosa para cachorro de porte grande",
         preco: 199.99,
         cliente: false,
-        img: "/src/assets/7.png"
+        img: "assets/7.png"
     },
     {
         idServico: 8,
@@ -61,6 +61,6 @@ export const dataServicos = [
         servico: "Tosa para cachorro de porte grande",
         preco: 99.99,
         cliente: true,
-        img: "/src/assets/8.png"
+        img: "assets/8.png"
     },
 ];
