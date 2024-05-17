c7771158260cb2594ef0aa4c86c2b4c47b1ec702
Rocco Caco <32roccocaco@gmail.com>
2024-05-17 19:52:02 -0300

    update

--- pages/login/script.js
+++ pages/login/script.js
@@ -9,29 +9,36 @@ buttonSubmit.addEventListener("click", function (event) {
   const password = inputPassword.value;
 
    // Validar e-mail
+   // se nao tiver um email, insira um. 
    if (!email) {
     alert("Por favor, insira um e-mail.");
     return;
+    // se nao for um email valido, o email inválido é sem @.com.
   } else if (!validateEmail(email)) {
     alert("Por favor, insira um e-mail válido.");
     return;
   }
 
   // Validar senha
+  // se o campo password tiver em branco.
   if (!password) {
     alert("Por favor, insira uma senha.");
     return;
+// se tiver menos que 6 caracteres, nao aceita.
   } else if (password.length < 6) {
     alert("A senha deve ter pelo menos 6 caracteres.");
     return;
   }
 
+  // o email e password tem que ser iguais.
   const usuario = usuarios.find(
     (usuario) => usuario.email === email && usuario.password === password
   );
 
+  // se o usuario for encontrado, irá para pagina inicial.
   if (usuario) {
     return (window.location.href = "../../index.html");
+  // se nao for encontrado.
   } else {
     alert("usuario nao encontrado");
   }
