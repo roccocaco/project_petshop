44d92a75f677d94068122edf0357b8b19def3550
Rocco Caco <32roccocaco@gmail.com>
2024-07-05 22:32:18 -0300

    update

--- pages/login/script.js
+++ pages/login/script.js
@@ -1,49 +1,51 @@
+// Carrega os usuários do localStorage
 let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+
+// Seleciona os elementos do formulário e do botão de envio
 const inputEmail = document.getElementById("input-email");
 const inputPassword = document.getElementById("input-password");
 const buttonSubmit = document.getElementById("button-submit");
 
+// Adiciona um evento de clique ao botão de envio
 buttonSubmit.addEventListener("click", function (event) {
-  event.preventDefault();
+  event.preventDefault(); // Previne o comportamento padrão do formulário
+
+  // Obtém o valor dos campos de e-mail e senha
   const email = inputEmail.value;
   const password = inputPassword.value;
 
-   // Validar e-mail
-   // se nao tiver um email, insira um. 
-   if (!email) {
+  // Validar e-mail
+  if (!email) {
     alert("Por favor, insira um e-mail.");
     return;
-    // se nao for um email valido, o email inválido é sem @.com.
   } else if (!validateEmail(email)) {
     alert("Por favor, insira um e-mail válido.");
     return;
   }
 
   // Validar senha
-  // se o campo password tiver em branco.
   if (!password) {
     alert("Por favor, insira uma senha.");
     return;
-// se tiver menos que 6 caracteres, nao aceita.
   } else if (password.length < 6) {
     alert("A senha deve ter pelo menos 6 caracteres.");
     return;
   }
 
-  // o email e password tem que ser iguais.
+  // Procurar pelo usuário no array de usuários
   const usuario = usuarios.find(
     (usuario) => usuario.email === email && usuario.password === password
   );
 
-  // se o usuario for encontrado, irá para pagina inicial.
+  // Se o usuário for encontrado, redireciona para a página inicial
   if (usuario) {
-    return (window.location.href = "../../index.html");
-  // se nao for encontrado.
+    window.location.href = "../../index.html";
   } else {
-    alert("usuario nao encontrado");
+    alert("Usuário não encontrado.");
   }
 });
 
+// Função para validar o formato do e-mail usando expressão regular
 function validateEmail(email) {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(email);
