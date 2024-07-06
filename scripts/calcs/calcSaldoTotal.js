aad4240839e0bebc0d0a2ffacaff918d8a91eaa5
Rocco Caco <32roccocaco@gmail.com>
2024-07-05 22:37:11 -0300

    update

--- scripts/calcs/calcSaldoTotal.js
+++ scripts/calcs/calcSaldoTotal.js
@@ -1,7 +1,12 @@
+// Importa a função calcMensalidade do arquivo calcMensalidade.js
 import { calcMensalidade } from "./calcMensalidade.js";
+
+// Importa a função calcServicos do arquivo calcServicos.js
 import { calcServicos } from "./calcServicos.js";
 
-
+// Função para calcular o saldo total, somando o resultado das funções calcMensalidade e calcServicos
 export function calcSaldoTotal() {
-    return (calcMensalidade() + calcServicos()).toFixed(2);
-};
+  // Calcula o saldo total somando o resultado das funções calcMensalidade e calcServicos
+  // O método toFixed(2) é utilizado para arredondar o resultado para duas casas decimais
+  return (calcMensalidade() + calcServicos()).toFixed(2);
+}
