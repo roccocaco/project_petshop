fa5640294b713cc06758539fa58cdb72c07699fb
Rocco Caco <32roccocaco@gmail.com>
2024-07-05 22:38:26 -0300

    update

--- scripts/calcs/calcServicos.js
+++ scripts/calcs/calcServicos.js
@@ -1,9 +1,13 @@
+// Importa o array de dados dataVendas do arquivo dataVendas.js
 import { dataVendas } from "../../data/dataVendas.js";
 
+// Função para calcular o saldo total dos serviços vendidos
 export function calcServicos() {
   let saldo = 0;
 
+  // Itera sobre o array dataVendas
   for (let index = 0; index < dataVendas.length; index++) {
+    // Condições para calcular o saldo com base no tipo de serviço vendido
     if (dataVendas[index].tipoServico === 1) {
       saldo += 49.99;
     }
@@ -36,6 +40,7 @@ export function calcServicos() {
       saldo += 99.99;
     }
   }
+
+  // Retorna o saldo total calculado
   return saldo;
 }
-
